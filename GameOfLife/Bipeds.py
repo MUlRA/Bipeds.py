335c2c832ed0ced24bc92dc6592926b9cec5dc90
alex <alexandremuir@gmail.com>
2022-04-07 13:53:39 -0400

    Deprecate, please refer to version 1.03

--- GameOfLife/Bipeds.py
+++ GameOfLife/Bipeds.py
@@ -27,11 +27,35 @@ class Population:
         fgend_list.sort()
         return fgend_list
 
-    def filter_generation(self, value):
+    def filter_generation(self, gen):
         """
-        filter_generation must go through all humans in population and make a new list of humans to return if they match the given generation.
-        It has one generation
-        """
+             filter_generation must go through all humans in population and make a new list of humans to return if they match the given generation.
+             It has one generation
+             """
+
+        #key, value
+        gen = gen.lower()
+        gen_list = []
+        gen_mapping = {
+                'genx': range(1965, 1980),
+                'genz': range(1997, 2012),
+                'gena': range(2010, 2020),
+                'mill': range(1981, 1996)
+            }
+        for key, r in gen_mapping.items():
+            if key in gen.lower():
+                print(r)
+                for people in self.pop_list:
+                    if people.birthday in r:
+                        gen_list.append(people)
+                        print(people.name)
+                    else:
+                        continue
+                return gen_list
+        print(gen_mapping[self])
+
+    def (self, value):
+
         value = value.lower()
         genx_list = []
         # 1965-80
@@ -41,11 +65,29 @@ class Population:
         # 1997-12
         gena_list = []
         # 2010-20
+
+        #key, value. Mapper|Dictionary
+        gen_mapping = {
+            'genx': range(1965, 1980),
+            'genz': range(1997, 2012),
+            'gena': range(2010, 2020),
+            'mill': range(1981, 1996)
+        }
+        if "auto" in value.lower():
+            for people in self.pop_list:
+                for key, z in gen_mapping.items():
+                    if people.birthday in z:
+                        genx_list.append(people.name)
+            return genx_list
+        else:
+            return genx_list
+            pprint("There is no human in the given list!")
+
         if "x" in value:
             for people in self.pop_list:
                 if people.birthday in range(1965, 1980):
-                    genx_list.append(people.name)
-                    return genx_list
+                    genx_list.append(people)
+                return genx_list
 
             else:
                 return "There is no Gen X in the given list!"
@@ -54,7 +96,7 @@ class Population:
             for people in self.pop_list:
                 if people.birthday in range(1981, 1996):
                     mill_list.append(people.name)
-                    return mill_list
+                return mill_list
 
             else:
                 return "There is no millennial in the given list!"
@@ -63,7 +105,7 @@ class Population:
             for people in self.pop_list:
                 if people.birthday in range(2010, 2020):
                     gena_list.append(people.name)
-                    return gena_list
+                return gena_list
 
             else:
                 return "There is no Gen Alpha in the given list!"
@@ -72,7 +114,7 @@ class Population:
             for people in self.pop_list:
                 if people.birthday in range(1997, 2012):
                     genz_list.append(people.name)
-                    return genz_list
+                return genz_list
 
             else:
                 return "There is no Gen Z in the given list"
@@ -98,4 +140,4 @@ people_list = [
 ]
 
 population = Population(people_list)
-pprint(population.filter_gender("MALE"))
+pprint(population.find("mill"))
