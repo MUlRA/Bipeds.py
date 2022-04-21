0d2a0dcf1a46dfb499b5120949415dfa0b63aec5
goujin <jimmy.goulet93@gmail.com>
2022-04-20 22:04:43 -0400

    Initial biped implementation with Human and Population (#1)
    * Revert "blanking the file for a pr review"
    
    This reverts commit 50b95fe2bc4ab81f803100cff07f96b8c1d602a3.
    
    * Change to the returned value (Pointer instead of names.)
    Change to the way generation is iterated through, (mapper instead of if loops)
    changed some indentations. Sorting no longer work with the pointers.
    
    * removed troubleshooting print calls and the else continue.
    
    * Deprecate, please refer to version 1.03
    
    * Overwriten the original file with code from the 1.03
    
    * Delete Bipeds_1.03.py
    
    deleted for git and moved code to orgine file
    
    * Add files via upload
    
    version 1.03 code for Bipeds, reupload
    
    * commit fiiiiiiiiiiiiiiiile! :3
    
    * commit folder
    
    Co-authored-by: alex <alexandremuir@gmail.com>
    Co-authored-by: MUlRA <84465314+MUlRA@users.noreply.github.com>

---
+++ python/GameOfLife/Bipeds.py
@@ -0,0 +1,66 @@
+from pprint import pprint
+
+
+class Human:
+    def __init__(self, name, birthday, gender):
+        self.name = name
+        self.birthday = birthday
+        self.gender = gender
+
+
+class Population:
+    def __init__(self, humans):
+        self.pop_list = humans
+
+    def filter_gender(self, value):
+        fgend_list = []
+        value = value.lower()
+        for people in self.pop_list:
+            if people.gender.lower() == value:
+                fgend_list.append(people)
+        return fgend_list
+
+    def filter_generation(self, gen):
+        """
+             filter_generation must go through all humans in population and make a
+              new list of humans to return if they match the given generation.
+             It has one generation
+             """
+
+        # key, value
+        hum_gen_list = []
+        gen_mapping = {
+            'genx': range(1965, 1980),
+            'genz': range(1997, 2012),
+            'gena': range(2010, 2020),
+            'mill': range(1981, 1996)
+        }
+        for key, gen_range in gen_mapping.items():
+            if key == gen.lower():
+                for people in self.pop_list:
+                    if people.birthday in gen_range:
+                        hum_gen_list.append(people)
+                return hum_gen_list
+
+    def sort_name(self):
+        sorted_list = []
+        for people in self.pop_list:
+            sorted_list.append(people)
+        sorted_list.sort()
+        return sorted_list
+
+
+people_list = [
+    Human("Bob", 2006, "Robot"),
+    Human("Cath", 1980, "Female"),
+    Human("Zach", 2012, "Male"),
+    Human("Alice", 1995, "Female"),
+]
+
+population = Population(people_list)
+population.filter_gender('female')
+""" 
+'genx': range(1965, 1980),
+'genz': range(1997, 2012),
+'gena': range(2010, 2020),
+'mill': range(1981, 1996)"""
